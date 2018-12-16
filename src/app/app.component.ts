aa773fbae22591c35cef45ae7d71e5290343e76c
Hendi Santika <hendisantika@gmail.com>
2018-12-16 07:30:37 +0700

    #14 - Modify app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PolicyService} from './policy.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
 })
-export class AppComponent {
-  title = 'angular7-inmemory-api';
+
+export class PolicyListComponent implements OnInit {
+
+  policies: any[] = [];
+
+  constructor(private policyService: PolicyService) {
+  }
+
+  ngOnInit() {
+    this.policyService.getPolicies().subscribe((data: any[]) => {
+      console.log(data);
+      this.policies = data;
+    });
+  }
 }
