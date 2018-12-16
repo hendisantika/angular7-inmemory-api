dd5d2447eb71ee7ec3687b85d3ed0c08d75639ed
Hendi Santika <hendisantika@gmail.com>
2018-12-16 07:28:56 +0700

    #13 - Creating our Data CRUD Service

---
+++ src/app/policy.service.ts
@@ -0,0 +1,54 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PolicyService {
+  SERVER_URL: string = 'http://localhost:8080/api/';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  public getPolicies() {
+    return this.httpClient.get(this.SERVER_URL + 'policies');
+  }
+
+  public getPolicy(policyId) {
+    return this.httpClient.get(`${this.SERVER_URL + 'policies'}/${policyId}`);
+  }
+
+  public createPolicy(policy: { id: number, amount: number, clientId: number, userId: number, description: string }) {
+    return this.httpClient.post(`${this.SERVER_URL}` + 'policies'
+  }
+
+)
+}
+
+public
+deletePolicy(policyId);
+{
+  return this.httpClient.delete(`${this.SERVER_URL + 'policies'}/${policyId}`);
+}
+
+public
+updatePolicy(policy;
+:
+{
+  number, amount;
+:
+  number, clientId;
+:
+  number, userId;
+:
+  number, description;
+:
+  string;
+}
+)
+{
+  return this.httpClient.put(`${this.SERVER_URL + 'policies'}/${policy.id}`);
+}
+
+
+}
