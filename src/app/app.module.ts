4b00b21e0f76d7f94293c5140330b2c6904af959
Hendi Santika <hendisantika@gmail.com>
2018-12-16 07:23:56 +0700

    #12 - Modify app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {DataService} from './data.service';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InMemoryWebApiModule.forRoot(DataService)
   ],
   providers: [],
   bootstrap: [AppComponent]
